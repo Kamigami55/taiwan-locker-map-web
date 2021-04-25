3d31b281772c06b7f9f198660bd461813a8203ef
Kamigami55 <eason@easonchang.com>
2021-04-25 22:43:28 +0800

    Update style

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 
 export default function IndexPage() {
   return (
-    <div className={styles.homePageContainer}>
+    <div className="flex flex-col h-screen">
       <Navbar />
       <iframe
-        className={styles.myGoogleMap}
+        className="relative flex-grow w-screen"
         src="https://www.google.com/maps/d/u/2/embed?mid=1gW8QWdqyhAODSdi235C60NqjwhuhZpdl"
         scrolling="no"
         frameBorder="0"
