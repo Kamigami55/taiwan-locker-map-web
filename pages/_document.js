55bf4e7ec6252b5cd19ac58f17a222f771e2c5dc
Kamigami55 <eason@easonchang.com>
2021-04-25 23:23:14 +0800

    Fix GA bug

--- pages/_document.js
+++ pages/_document.js
@@ -91,7 +91,7 @@ class MyDocument extends Document {
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
-            async
+            async="async"
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
